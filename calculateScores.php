760ec8dd0e0e4e0e9ef43df667a518950d4e5b0a
felix_boie <info@felix-boie.de>
2019-10-14 14:06:30 +0200

    function for calculating score should work

--- calculateScores.php
+++ calculateScores.php
@@ -5,30 +5,44 @@ $json = file_get_contents('argument.json');
 //Decode JSON
 $json_data = json_decode($json,true);
 
-//Start with the first Argument
-$firstArgument = $json_data['childs'][1];
 
-defArgument($firstArgument);
 
+//print main argument
+print($json_data['title']);
+echo '<br/>';
+
+//Start with the main Argument
+defArgument($json_data);
+
+// show final json file
 print_r($json_data);
 
 //Start the Function
-function defArgument($array) {
-$calculatedScore = 0;
-if ($array['calculatedScore'] > -1000) {
-    return $array['calculatedScore'];
-}
-elseif (empty($array['childs'])) {
-    $array['calculatedScore'] = 1;
-    return 1;
-}
-else {
-    
-    for ($i = 0; $i < $array['calculatedScore']; $i++) {
-    
-        $calculatedScore += defArgument($array['childs'][$i]) * $array['childs'][$i]['score'];
-    }
-    $array['calculatedScore'] = $calculatedScore;
-    return $calculatedScore;
-}
+function defArgument(&$array) {
+	
+	if ($array['calculatedScore'] > -1000) {
+		print("run part1");
+		echo '<br/>';
+		
+    	return $array['calculatedScore'];
+	}
+	elseif (empty($array['childs'])) {
+		print("run part2_works");
+		echo '<br/>';
+		
+	    $array['calculatedScore'] = 1;  
+	    return 1;
+	}
+	else {
+		print("run part3");
+		echo '<br/>';
+	    // loop over all children in the next level
+		$calculatedScore = 0;
+	    for ($i = 0; $i < $array['answerCount']; $i++) {
+	    
+	        $calculatedScore += defArgument($array['childs'][$i]) * $array['childs'][$i]['score'];
+	    }
+	    $array['calculatedScore'] = $calculatedScore;
+	    return $calculatedScore;
+	}
 }
