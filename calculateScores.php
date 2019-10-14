9c2b9af2e95e0a88da9c9618197d22a839f110cc
felix_boie <info@felix-boie.de>
2019-10-14 15:46:45 +0200

    added pathCounts and AcceptabilityDegree(AD). AD should be used to define size of arguments in our visualization. (it is always betwen 0 and 2)

--- calculateScores.php
+++ calculateScores.php
@@ -21,28 +21,38 @@ print_r($json_data);
 function defArgument(&$array) {
 	
 	if ($array['calculatedScore'] > -1000) {
+		// aguments calculatedScore and path have alreay been defined
 		print("run part1");
 		echo '<br/>';
 		
     	return $array['calculatedScore'];
 	}
 	elseif (empty($array['childs'])) {
-		print("run part2_works");
+		// argument is at the most outer level, calculatedScore, path and acceptabilityDegree are set to 1. 
+		print("run part2");
 		echo '<br/>';
 		
-	    $array['calculatedScore'] = 1;  
+	    $array['calculatedScore'] = 1;
+	    $array['pathCount'] = 1;
+	    $array['acceptabilityDegree'] = 1;
 	    return 1;
 	}
 	else {
+		// argument needs to be defined, and is not on the most outer level
 		print("run part3");
 		echo '<br/>';
 	    // loop over all children in the next level
 		$calculatedScore = 0;
+		$pathCount = 0;
 	    for ($i = 0; $i < $array['answerCount']; $i++) {
-	    
-	        $calculatedScore += defArgument($array['childs'][$i]) * $array['childs'][$i]['score'];
+	    	//order is important, first the calculatedScore needs to run, then the count.
+	        $calculatedScore += defArgument($array['childs'][$i]) * $array['childs'][$i]['score']/100; // needs to be divided by 100, as scores should be between -1 and 1
+	        $pathCount += $array['childs'][$i]['pathCount'];
+	        
 	    }
 	    $array['calculatedScore'] = $calculatedScore;
+	    $array['pathCount'] = $pathCount;
+	    $array['acceptabilityDegree'] = 1 + $calculatedScore/$pathCount;
 	    return $calculatedScore;
 	}
 }
--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 // $question_url = "https://www.kialo.com/2629";    
-$question_url = "https://www.kialo.com/30673";
+$question_url = "https://www.kialo.com/23956";
 ini_set('max_execution_time', 3000);
 $level = 0;
 $argObj = [];
@@ -9,6 +9,8 @@ $procon = 0;
 $score = 0;
 $reference = 0;
 $calculatedScore = -1000;
+$pathCount = 0;  //how many paths from the most outer level lead to this argument
+$acceptabilityDegree = 10; // a value between 0 and 2, will represent the size of arguments in visualization; 10 is set as the intial value
 $getTitle = 0;
 $count = 0;
 $level = 0;
@@ -28,7 +30,7 @@ $mystring = end($parts);
 echo $mystring;
 
 echo "<br><br>";
-$argObj2 = runArgument($question_url, $procon, $score, $reference, $calculatedScore, $level, $maxLevel);
+$argObj2 = runArgument($question_url, $procon, $score, $reference, $calculatedScore, $pathCount, $acceptabilityDegree, $level, $maxLevel);
 
 $argObj2->color = "#292929";
 $argObj2->score = 20;
@@ -39,7 +41,7 @@ $fp = fopen('argument.json', 'w');
 fwrite($fp, json_encode($argObj2));
 fclose($fp);
 
-function runArgument($question_url, $procon, $score, $reference, $calculatedScore, $level, $maxLevel) {
+function runArgument($question_url, $procon, $score, $reference, $calculatedScore, $pathCount, $acceptabilityDegree, $level, $maxLevel) {
     $level++;
 
     $data = file_get_contents($question_url);
@@ -63,6 +65,8 @@ function runArgument($question_url, $procon, $score, $reference, $calculatedScor
     $argObj->score = $score;
     $argObj->reference = $reference;
     $argObj->calculatedScore = $calculatedScore;
+    $argObj->pathCount = $pathCount;
+    $argObj->acceptabilityDegree = $acceptabilityDegree;
     
     $outerChild = []; 
     $argNr = 0;
@@ -75,7 +79,9 @@ function runArgument($question_url, $procon, $score, $reference, $calculatedScor
             $score_inner = $argument['upvoteCount'];
             $reference_inner = substr(explode('active=', $argument['url'], 2)[1],1);
             $calculatedScore_inner = -1000;
-            $outerChild[] = runArgument("https://www.kialo.com/$reference_inner", $procon_inner, $score_inner, $reference_inner, $calculatedScore_inner, $level, $maxLevel);
+            $pathCount_inner = 0;
+            $acceptabilityDegree_inner = 10;
+            $outerChild[] = runArgument("https://www.kialo.com/$reference_inner", $procon_inner, $score_inner, $reference_inner, $calculatedScore_inner, $pathCount_inner, $acceptabilityDegree_inner, $level, $maxLevel);
             $argNr++;
         }
         $argObj->childs = json_decode(json_encode($outerChild));
@@ -88,7 +94,7 @@ function runArgument($question_url, $procon, $score, $reference, $calculatedScor
     // return json_decode(json_encode($argObj));   
 }
 
-
+// I think this is no longer needed (Felix) ???
 function defArgument($argument) {
     var_dump(json_decode($argument->ModuleAccountInfo));
     var_dump($argument);
